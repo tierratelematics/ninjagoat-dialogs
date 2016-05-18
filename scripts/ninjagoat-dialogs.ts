56daf6b6819d570ef660ccc549e4ddea4646df1c
Mattia Franchetto <mt.franchetto@gmail.com>
2016-05-18 18:19:57 +0200

    Remove interfaces from exports

--- scripts/ninjagoat-dialogs.ts
+++ scripts/ninjagoat-dialogs.ts
@@ -1,14 +1,6 @@
 /// <reference path="../typings/index.d.ts" />
 import DialogsModule from "./DialogsModule";
-import IDialogService from "./IDialogService";
-import IAlertService from "./IAlertService";
-import IConfirmationService from "./IConfirmationService";
-import ICustomDialogService from "./ICustomDialogService";
 import SimpleDialogService from "./SimpleDialogService";
 
 export {DialogsModule}
-export {IDialogService}
-export {IAlertService}
-export {IConfirmationService}
-export {ICustomDialogService}
 export {SimpleDialogService}
