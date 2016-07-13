e80c21a033adb6f27318c6976b9610f0452f63f2
Mattia Franchetto <mt.franchetto@gmail.com>
2016-07-13 10:22:05 +0200

    Remove registration keys

--- scripts/DialogsModule.ts
+++ scripts/DialogsModule.ts
@@ -1,16 +1,13 @@
 import {IModule} from "ninjagoat";
 import {IKernel, IKernelModule} from "inversify";
 import IDialogService from "./interfaces/IDialogService";
-import SimpleDialogService from "./SimpleDialogService";
 import {IViewModelRegistry} from "ninjagoat";
 import {IServiceLocator} from "ninjagoat";
-import * as RegistrationKeys from "./RegistrationKeys";
 import NinjagoatDialogService from "./components/NinjagoatDialogService";
 
 class DialogsModule implements IModule {
 
     modules:IKernelModule = (kernel:IKernel) => {
-        kernel.bind<IDialogService>("IDialogService").to(SimpleDialogService).inSingletonScope().whenTargetNamed(RegistrationKeys.Simple_Dialog);
         kernel.bind<IDialogService>("IDialogService").to(NinjagoatDialogService).inSingletonScope();
     };
 
--- scripts/ninjagoat-dialogs.ts
+++ scripts/ninjagoat-dialogs.ts
@@ -4,11 +4,9 @@ import SimpleDialogService from "./SimpleDialogService";
 import NinjagoatDialog from "./components/NinjagoatDialog";
 import NinjagoatDialogService from "./components/NinjagoatDialogService";
 import CustomDialog from "./components/CustomDialog";
-import * as RegistrationKeys from "./RegistrationKeys";
 
 export {DialogsModule}
 export {SimpleDialogService}
 export {NinjagoatDialog}
 export {NinjagoatDialogService}
-export {RegistrationKeys}
 export {CustomDialog}
